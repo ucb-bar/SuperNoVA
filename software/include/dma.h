b6c69079b147b28f9c262c8b8f306375fc775517
SeahK <kimsea8209@berkeley.edu>
2024-05-22 11:52:11 -0700

    update header

--- software/include/dma.h
+++ software/include/dma.h
@@ -40,11 +40,18 @@
 
 // base source & dest address, stride
 // mode: memcpy in / out
-#define dma_source_config(channel, bypass_sbus, source_addr, source_stride) \
+#define dma_nord_source_config(channel, bypass_sbus, source_addr, source_stride) \
   ROCC_INSTRUCTION_RS1_RS2(XCUSTOM_DMA, ((uint64_t) source_stride << 5) | ((uint64_t) channel), ((uint64_t) bypass_sbus << 63) | ((uint64_t) source_addr), k_CONFIG_S)
 
-#define dma_dest_config(channel, bypass_sbus, dest_addr, dest_stride) \
+#define dma_nord_dest_config(channel, bypass_sbus, dest_addr, dest_stride) \
   ROCC_INSTRUCTION_RS1_RS2(XCUSTOM_DMA, ((uint64_t) dest_stride << 5) | ((uint64_t) channel), ((uint64_t) bypass_sbus << 63) | ((uint64_t) dest_addr), k_CONFIG_D)
+
+
+#define dma_source_config(channel, granted, bypass_sbus, source_addr, source_stride) \
+  ROCC_INSTRUCTION_RD_RS1_RS2(XCUSTOM_DMA, granted, ((uint64_t) source_stride << 5) | ((uint64_t) channel), ((uint64_t) bypass_sbus << 63) | ((uint64_t) source_addr), k_CONFIG_S)
+
+#define dma_dest_config(channel, granted, bypass_sbus, dest_addr, dest_stride) \
+  ROCC_INSTRUCTION_RD_RS1_RS2(XCUSTOM_DMA, granted, ((uint64_t) dest_stride << 5) | ((uint64_t) channel), ((uint64_t) bypass_sbus << 63) | ((uint64_t) dest_addr), k_CONFIG_D)
 
 // copy 1 tile
 // granted: return value whether DMA granted the requested ISA
@@ -61,6 +68,13 @@
 #define dma_memcpy_multitile(channel, granted, source_offset, dest_offset, index, inter_tile_source_offset, inter_tile_dest_offset, num_tile, tile_rows, tile_bytes_per_row) \
   ROCC_INSTRUCTION_RD_RS1_RS2(XCUSTOM_DMA, granted, ((uint64_t) channel << 60) | ((uint64_t) index << 54) | ((uint64_t) tile_rows << 46) | ((uint64_t) tile_bytes_per_row << 34) | ((uint64_t) inter_tile_dest_offset << 17) | ((uint64_t) inter_tile_source_offset), ((uint64_t) num_tile << 56) | ((uint64_t) dest_offset << 32) | ((uint64_t) source_offset), k_MEMCPY_MULTI)
 
+#define dma_nord_memcpy_tile(channel, source_offset, dest_offset, index, tile_rows, tile_bytes_per_row) \
+  ROCC_INSTRUCTION_RS1_RS2(XCUSTOM_DMA, ((uint64_t) channel << 60) | ((uint64_t) index << 52) | ((uint64_t) tile_rows << 32) | ((uint64_t) dest_offset), ((uint64_t) source_offset << 32) | ((uint64_t) tile_bytes_per_row), k_MEMCPY)
+
+#define dma_nord_memcpy_multitile(channel, source_offset, dest_offset, index, inter_tile_source_offset, inter_tile_dest_offset, num_tile, tile_rows, tile_bytes_per_row) \
+  ROCC_INSTRUCTION_RS1_RS2(XCUSTOM_DMA, ((uint64_t) channel << 60) | ((uint64_t) index << 54) | ((uint64_t) tile_rows << 46) | ((uint64_t) tile_bytes_per_row << 34) | ((uint64_t) inter_tile_dest_offset << 17) | ((uint64_t) inter_tile_source_offset), ((uint64_t) num_tile << 56) | ((uint64_t) dest_offset << 32) | ((uint64_t) source_offset), k_MEMCPY_MULTI)
+
+
 // probe state register to know which tile is done
 // memcpy accelerator with set DMA state register to indicate up until which tile index has finished memcpy
 #define dma_probe_state(done_tile, channel) \
@@ -69,7 +83,9 @@
 // CPU writes finished state index
 // e.g. when accelerator output needs to be used by others, CPU sets the index of the tile that accel has finihsed mvout, so that the consumer can probe to track dependency without synchronization
 // since accelerator cannot set DMA channel's state register, unlike memcpy accelerator
-#define dma_set_state(done_tile, channel) \
+#define dma_set_state(grant, done_tile, channel) \
+  ROCC_INSTRUCTION_RD_RS1_RS2(XCUSTOM_DMA, granted, channel, done_tile, k_SET)
+#define dma_nord_set_state(done_tile, channel) \
   ROCC_INSTRUCTION_RS1_RS2(XCUSTOM_DMA, channel, done_tile, k_SET)
 
 // maybe not needed once we have rerocc
@@ -77,4 +93,3 @@
   ROCC_INSTRUCTION_RS1_RS2(XCUSTOM_DMA, skip, 0, k_FLUSH)
 
 #endif // SRC_MAIN_C_DMA_H
-
